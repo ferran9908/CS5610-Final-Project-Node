7b180da53124cfe46372c2369bd703fd99cacb79
Ferran <ferran.9908@gmail.com>
2022-12-11 03:38:34 -0800

    Make HouseId optional

--- models/houseImages.js
+++ models/houseImages.js
@@ -4,21 +4,21 @@ import Mongoose from "mongoose";
  * Schema for House Images collection in mongo db
  */
 const houseImagesSchema = new Mongoose.Schema({
-    pic: {
-        type: String,
-        required: false
-    },
-    houseId: {
-        type: String,
-        required: true
-    }
+  pic: {
+    type: String,
+    required: false,
+  },
+  houseId: {
+    type: String,
+  },
 });
 
-houseImagesSchema.virtual('id', () => this._id.toHexString());
+houseImagesSchema.virtual("id", () => this._id.toHexString());
 
-houseImagesSchema.set('toJSON', { virtuals: true });
+houseImagesSchema.set("toJSON", { virtuals: true });
 
 //Images collection
-const Images = Mongoose.model('Image', houseImagesSchema);
+const Images = Mongoose.model("Image", houseImagesSchema);
 
 export default Images;
+
